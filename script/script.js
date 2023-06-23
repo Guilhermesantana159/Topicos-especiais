0da106545d0d2be724ebf7f258a3a48bc34683cb
Guilherme Pereira santana <guilhermesantana159357@hotmail.com>
2023-06-23 20:13:27 -0300

    merge
    merge

--- script/script.js
+++ script/script.js
@@ -7,6 +7,62 @@ var frases = [
 
 var index = 0;
 var fraseElement = document.getElementById("frase");
+var cores = ["#ccf654","white"];
+var coreVez = 0;
+
+function OnInit(){
+    let isLogado = window.localStorage.getItem('Nome');
+
+    if(isLogado == undefined || isLogado == 'undefined'){
+        document.getElementById('login').style.display = "block";
+        document.getElementById('sair').style.display = "none";
+    }else{
+        document.getElementById('login').style.display = "none";
+        document.getElementById('sair').style.display = "block";
+    }
+
+    setInterval(function() {
+        document.getElementById('pisca-pisca').style.backgroundColor = cores[coreVez];
+        coreVez +=1;
+
+        if(coreVez > 1){
+            coreVez = 0;
+        }
+    }, 100);
+};
+
+
+function Logar(event){
+    event.preventDefault(); 
+
+    var username = document.getElementById('username');
+    var password = document.getElementById('password');
+
+    if (username.value === 'admin' && password.value === '12345') {
+        alert('Login bem-sucedido!');
+        localStorage.setItem("Nome",username.value);
+        location.href = window.location.pathname.replace("login","index");
+    } 
+    else {
+      alert('Nome de usuário ou senha inválidos!');
+      username.value = "";
+      password.value = "";
+    }
+};
+
+function Cadastrar(event){
+    event.preventDefault(); 
+
+    alert('Cadastro feito com sucesso!')
+    localStorage.setItem("Nome",username.value);
+    location.href = window.location.pathname.replace("register","index");
+};
+
+function Deslogar(){
+    window.localStorage.setItem('Nome',undefined);
+    location.href = "file:///C:/Source/Topicos-especiais/index.html"
+}
+
 
 function exibirProximaFrase() {
     fraseElement.textContent = frases[index];
